85795e158208d7a7ec79a3c18ba28d13ad6e3ff9
Luca <gabadadzeluca@gmail.com>
2022-11-14 15:19:41 +0400

    add winner detection

--- game.js
+++ game.js
@@ -10,19 +10,13 @@ const winPatterns = [
     [3,5,7]
 ]
 
+let gameActive = true;
+
 
 const restartElements = document.querySelectorAll('.restart');
 
 const grid = document.querySelector('#grid').childNodes;
-//add cross to the grid-square
-grid.forEach(box => {
-    box.addEventListener('click', ()=>{
-        console.log("clicked" ,box.id);
-        //test
-        box.classList.add('cross');
-        // box.classList.add('circle');
-    })
-});
+
 
 // get elements 
 
@@ -35,7 +29,7 @@ const cpuBtn = document.querySelector('#cpu-btn');
 const playerBtn = document.querySelector('#player-btn');
 //choosing player 1's mark
 const playerChoice = document.querySelector('#p1-choice').childNodes; 
-console.log(playerChoice);
+console.log("player choice: ",playerChoice);
 const squareIcon = document.querySelector('.x');
 const circleIcon = document.querySelector('.o');
 
@@ -47,10 +41,10 @@ const circleIcon = document.querySelector('.o');
 
 // multiplayer or solo
 cpuBtn.addEventListener('click', ()=>{
-    game('cpu');
+    game('solo');
 });
 playerBtn.addEventListener('click', ()=>{
-    game('player');
+    game('multiplayer');
 });
 
 //grid element
@@ -80,8 +74,71 @@ function endGameScreen(){
     startScreen.style.display = 'none';
 }
 
+
+
+
+// check the current condition of board
+function checkGrid(){
+    document.addEventListener('click', ()=>{ 
+        // loop through the patterns
+        winPatterns.forEach(win=>{
+            // detect player's win
+            if(document.getElementById(win[0]).classList == 'cross' && 
+            document.getElementById(win[1]).classList == 'cross' &&
+            document.getElementById(win[2]).classList == 'cross'){
+                console.log('cross wins!!!');
+                gameActive = false;
+            }
+            if(document.getElementById(win[0]).classList == 'circle' && 
+            document.getElementById(win[1]).classList == 'circle' &&
+            document.getElementById(win[2]).classList == 'circle'){
+                console.log('circle wins!!!');
+                gameActive = false;
+            }
+        });
+
+    });
+
+
+}
+
 function multiplayerGame(){
-    
+    const playerOne = document.querySelector('.player-1');
+    console.log(playerOne.id);
+    let playerOneChoice = playerOne.id.substring(7);
+    let playerTwoChoice;
+    if(playerOneChoice == 'cross'){
+        playerTwoChoice = 'circle';
+    }else{
+        playerTwoChoice = 'cross';
+    }
+    checkGrid();
+    // add player icons to the grid and switch between users
+    let i = 0;
+    grid.forEach(box => {
+        box.addEventListener('click', function turns(){
+            console.log("clicked" ,box.id);
+            console.log('clicks: ', i);
+            if(box.classList != 'circle' && box.classList != 'cross'){
+                i++;
+                if(i % 2 == 0){
+                    console.log('i:', i);
+                    // add class chosen by player one
+                    box.classList.add(`${playerOne.id.substring(7)}`);
+                }else{
+                    box.classList.add(playerTwoChoice);
+                }
+                if(i == 9){
+                    gameActive = false;
+                    console.log('game is over');
+                    //stop loop
+                    // box.removeEventListener('click', turns);
+                }
+            }
+
+        })
+    });
+
 }
 
 
@@ -90,7 +147,7 @@ function multiplayerGame(){
 startGameScreen();
 
 function game(x){
-    console.log(x);
+    console.log("game: ",x);
 
     // load game
     mainGameScreen();
