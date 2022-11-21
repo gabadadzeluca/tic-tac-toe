0e08da663b6dd97e4942a842d15037cab8e2e1c9
Luca <gabadadzeluca@gmail.com>
2022-11-22 00:40:37 +0400

    add p1 toggle

--- game.js
+++ game.js
@@ -9,3 +9,72 @@ const winPatterns = [
     [1,5,9],
     [3,5,7]
 ]
+
+
+//starting points
+let circlePoints = 0;
+let crossPoints = 0;
+let countTies = 0;
+//who is to play
+let circleTurn;
+const crossCls = 'cross';
+const circleCls = 'circle';
+
+
+
+// starting screen
+const startScreen = document.getElementById('start-board');
+console.log(startScreen);
+
+// game screen
+const gameScreen = document.getElementById('game-board');
+
+//board element 
+const board = document.querySelectorAll('#grid div');
+console.log(board);
+
+// end screen
+const endScreen = document.getElementById('end-screen');
+
+
+
+// show starting screen and hide other elements
+function showStartMenu(){
+    startScreen.style.display = 'flex';
+    gameScreen.style.display = 'none';
+    endScreen.style.display = 'none';
+}
+
+function showGameScreen(){
+    startScreen.style.display = 'none';
+    gameScreen.style.display = 'flex';
+    endScreen.style.display = 'none';
+}
+
+function messageScreen(){
+    endScreen.style.display = 'flex';
+}
+// start game
+function startGame(){
+    showStartMenu();
+}
+
+// choosing player elements
+const playerX = document.querySelector('.x');
+const playerO = document.querySelector('.o');
+
+playerX.addEventListener('click', chooseP1)
+playerO.addEventListener('click', chooseP1)
+
+// toggle between icons
+function chooseP1(e){
+    choice = e.target;
+    console.log(choice);
+    document.querySelectorAll('#p1-choice div').forEach(div=>{
+        div.classList.remove('player-1');
+    })
+    choice.classList.add('player-1');
+}
+
+
+startGame();
